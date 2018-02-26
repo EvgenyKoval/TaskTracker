fe9c102d9f13b2fdcb841fa1b6940aa1fddec381
EvgenyKoval <shiller1993@gmail.com>
2018-02-26 09:40:41 +0300

    create react component

--- frontend/react-app/src/pages/App1.js
+++ frontend/react-app/src/pages/App1.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import axios from 'axios';
 
 class App1 extends React.Component {
-	render() {
-		const {groups} = {
-			"userId": 1,
-			"id": 1,
-			"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-			"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-		};
-		return <div>{groups}</div>
+
+	state = {
+		user: {
+			id: 1,
+			name: "name",
+			email: "",
+			role: 1,
+			company: {name: "company"}
+		}
+	};
+
+
+// var id = this.props.comment;
+	input() {
+		var id = document.getElementById('id').value;
+		this.state.user.id = id;
+		axios.get("https://jsonplaceholder.typicode.com/users/" + this.state.user.id)
+			.then((resp) => {
+				var data = resp.data;
+				console.log(data);
+				this.setState({user: data});
+			});
 	}
+
+	render() {
+		const user = this.state.user;
+		return <div>
+			Post id:
+			<input id="id" />
+			<button onClick={() => this.input()}>get</button>
+			<table>
+				<tr>
+					<th>User name</th>
+					<th>user ID</th>
+					<th>email</th>
+					<th>role id</th>
+				</tr>
+				<tr>
+					<td>{user.name}</td>
+					<td>{user.id}</td>
+					<td>{user.email}</td>
+					<td>{user.company.name}</td>
+				</tr>
+			</table>
+		</div>;
+	}
+
 }
 
 export default App1;
