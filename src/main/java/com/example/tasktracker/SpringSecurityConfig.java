e68acd0875c60e7d061fefc658c426fb60df71cb
EvgenyKoval <shiller1993@gmail.com>
2018-08-10 18:23:25 +0300

    spring security

---
+++ src/main/java/com/example/tasktracker/SpringSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.tasktracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AccessDeniedHandler accessDeniedHandler = new AccessDeniedHandler() {
+        @Override
+        public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/");
+        }
+    };
+
+    // роль admin всегда есть доступ к /admin/**
+    // роль user всегда есть доступ к /user/**
+    // Наш кастомный "403 access denied" обработчик
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/comments/**").hasAnyRole("Dev")
+                .antMatchers(HttpMethod.GET,"comments/comment/**").hasAnyRole("Dev")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    // создаем пользоватлелей, admin и user
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("dev").password("dev").roles("Dev");
+    }
+}
